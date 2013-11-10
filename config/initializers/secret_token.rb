e9ca58c88fd1c38dea7de473124baf8ee85bd164
Loganrun <bigdog_41@hotmail.com>
2013-11-10 00:01:03 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinnlearn::Application.config.secret_key_base = '1cdaaf6cfb7743ca8bdcd1bff11a6845ff220e85fd1f4ba1f1c5d28ee31052dc4ccb9df59d8a56ff71fd1d308e197dc5e532b6d4c288ba8bdf559ea285e7fd26'
